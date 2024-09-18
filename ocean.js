cf4668c7dde72f6d3683a1c01e28a5eac5f39e84
boravellicharan7 <boravellicharan6@gmail.com>
2024-09-18 18:11:43 +0530

    added

---
+++ ocean.js
@@ -0,0 +1,23 @@
+var father = "Balaraju";
+var age = 52;
+var job = "manager";
+var sal = "15k";
+var num = "8096xxxxxx";
+
+var mother = {
+    name: "sumalatha",
+    age: 42,
+    job: "teacher",
+    sal: "18k",
+    num: "9010xxxxxx"
+}
+
+var children = [child1={name:"lucky",age:14,study:"7th"},child2={name:"charan",age:21,study:"12th"}]
+
+console.log([children]);
+console.log(mother);
+console.log(father);
+console.log(age);
+console.log(job);
+console.log(sal);
+console.log(num);
